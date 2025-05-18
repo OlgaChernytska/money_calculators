7bd0939aa86079c8711b0061c73243d3c9dd1042
Artem <vertex451@gmail.com>
2025-05-18 12:16:06 +0200

    switcher style

--- frontend/src/components/i118/switcher.tsx
+++ frontend/src/components/i118/switcher.tsx
@@ -12,15 +12,22 @@ const handleChange = (event: SelectChangeEvent<string>) => {
 };
 
   return (
-    <FormControl variant="outlined" size="small">
+    <FormControl variant="outlined" size="small" sx={{ color: '#99d9d9', borderColor: '#99d9d9' }}>
       <Select
         value={i18n.language}
         onChange={handleChange}
         displayEmpty
         inputProps={{ 'aria-label': 'Select language' }}
+        sx={{
+          color: '#99d9d9',
+          '.MuiOutlinedInput-notchedOutline': { borderColor: '#99d9d9' },
+          '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: '#99d9d9' },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: '#99d9d9' },
+          '.MuiSelect-icon': { color: '#99d9d9' },
+        }}
       >
-        <MenuItem value="en">English</MenuItem>
-        <MenuItem value="ua">Українська</MenuItem>
+        <MenuItem value="en">EN</MenuItem>
+        <MenuItem value="ua">UA</MenuItem>
       </Select>
     </FormControl>
   );
