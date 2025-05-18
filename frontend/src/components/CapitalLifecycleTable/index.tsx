0d7733ed8e17bd0e1e4466389f6fb71bf9ff8101
Artem <vertex451@gmail.com>
2025-05-18 11:51:58 +0200

    locale at detailed info

--- frontend/src/components/CapitalLifecycleTable/index.tsx
+++ frontend/src/components/CapitalLifecycleTable/index.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { TableRowData } from '../../types';
+import { useTranslation } from 'react-i18next';
 
 interface TableProps {
   tableData: TableRowData[];
 }
 
 const CapitalLifecycleTable: React.FC<TableProps> = ({ tableData }) => {
+  const { t } = useTranslation();
   return (
     <Accordion>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography>Detailed Info</Typography>
+        <Typography>{t('detailed_info')}</Typography>
       </AccordionSummary>
       <AccordionDetails>
         <TableContainer component={Paper}>
           <Table> {/* This is the MUI Table component */}
             <TableHead>
               <TableRow>
-                <TableCell>Age</TableCell>
-                <TableCell align="right">Capital Start ($)</TableCell>
-                <TableCell align="right">Interest ($)</TableCell>
-                <TableCell align="right">Saved/Withdrawn ($)</TableCell>
-                <TableCell align="right">Capital End ($)</TableCell>
-                <TableCell align="right">Monthly Saved/Withdrawn ($)</TableCell>
+                <TableCell>{t('age')}</TableCell>
+                <TableCell align="right">{t('capital_start')}</TableCell>
+                <TableCell align="right">{t('interest')}</TableCell>
+                <TableCell align="right">{t('saved_withdrawn')}</TableCell>
+                <TableCell align="right">{t('capital_end')}</TableCell>
+                <TableCell align="right">{t('monthly_saved_withdrawn')}</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
